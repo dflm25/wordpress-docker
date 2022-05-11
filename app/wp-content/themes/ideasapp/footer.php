1226f9c7e13a5bd63623574e58f6830b91d5d226
Daniel Lucumi <dflm25@gmail.com>
2022-05-11 08:16:31 -0500

    created plugins

--- app/wp-content/themes/ideasapp/footer.php
+++ app/wp-content/themes/ideasapp/footer.php
@@ -3,57 +3,7 @@
       <div class="container">
         <div class="row padding-bottom-100">
           <div class="col-lg-6 col-md-12 col-sm-12 col-12">
-            <div class="contact-form-area">
-              <form action="#" class="contact-form">
-                <div class="form-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="name"
-                    name="name"
-                    placeholder="Your Name"
-                    required
-                  />
-                  <div class="label-box"></div>
-                </div>
-                <div class="form-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="email"
-                    name="email"
-                    placeholder="Your Mail"
-                    required
-                  />
-                  <div class="label-box"></div>
-                </div>
-                <div class="form-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="budget"
-                    name="budget"
-                    placeholder="Your Budget"
-                    required
-                  />
-                  <div class="label-box"></div>
-                </div>
-                <div class="form-group">
-                  <textarea
-                    class="form-control"
-                    rows="5"
-                    id="message"
-                    name="message"
-                    placeholder="Your Messages"
-                    required
-                  ></textarea>
-                  <div class="label-box"></div>
-                </div>
-                <div class="form-btn text-right">
-                  <button class="btn-1 contactsuccess">Hire Me</button>
-                </div>
-              </form>
-            </div>
+            <?php echo do_shortcode('[ideas_register]'); ?>
           </div>
           <div class="col-lg-6 col-md-12 col-sm-12 col-12">
             <div class="contact-details">
